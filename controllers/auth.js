59bd3e2748c165648388831098e30db02112fe8d
CalClemmer <cal1296@yahoo.com>
2021-09-03 16:19:43 -0700

    Ironed out bugs in auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,1 +1,13 @@
+const express = require('express');
+var router = express.Router();
 const passport = require('../config/ppConfig');
+
+router.get('/signup', (req, res) => {
+    res.render('auth/signup');
+  });
+  
+  router.get('/login', (req, res) => {
+    res.render('auth/login');
+  });
+
+module.exports = router; 
